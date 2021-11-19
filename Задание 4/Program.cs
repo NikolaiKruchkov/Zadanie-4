46854911076545e6f605800d1f3363cb1f2cb0ad
NikolaiKruchkov <47074017+NikolaiKruchkov@users.noreply.github.com>
2021-11-19 09:51:46 +0300

    Add files via upload

---
+++ Задание 4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Задание_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число N>0, затем нажмите Enter");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int Q = 0;
+            for (int i = 1; i < (2 * N + 1); i += 2)
+            {
+                Q = Q + i;
+                Console.WriteLine(Q);
+            }
+            Console.WriteLine("Квалрат числа N равен {0}",Q);
+
+
+        }
+    }
+}
